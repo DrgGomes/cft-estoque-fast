c4dd3c38465d70be239a4da3694b3eff8f7c51b7
null <null>
2026-03-27 10:37:09 -0300

    correcao 9

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Revendedor from './pages/Revendedor';
 import SuperAdmin from './pages/SuperAdmin';
 import { RefreshCw } from 'lucide-react';
 
-// O Roteador que decide qual tela mostrar com base no banco de dados
 function AppRouter() {
     const { globalLoading, isSuperAdminMode, superAdminAuthenticated, selectedRole } = useContext(AppContext);
 
@@ -27,7 +26,6 @@ function AppRouter() {
     return null;
 }
 
-// O App envelopa o sistema inteiro na Caixa D'água (AppProvider)
 export default function App() {
   return (
     <AppProvider>
